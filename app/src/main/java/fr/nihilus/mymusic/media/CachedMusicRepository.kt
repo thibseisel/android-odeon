3ad8298bca9e70ac628382976e7ee7ac7438dcec
Thibault Seisel <seisel.thibault@gmail.com>
2017-08-26 20:52:06 +0200

    Refactor: moved extension function to its own file

--- app/src/main/java/fr/nihilus/mymusic/media/CachedMusicRepository.kt
+++ app/src/main/java/fr/nihilus/mymusic/media/CachedMusicRepository.kt
@@ -1,12 +1,9 @@
 package fr.nihilus.mymusic.media
 
-import android.net.Uri
-import android.os.Bundle
-import android.provider.MediaStore
-import android.support.annotation.VisibleForTesting
 import android.support.v4.media.MediaBrowserCompat.MediaItem
 import android.support.v4.media.MediaDescriptionCompat
 import android.support.v4.media.MediaMetadataCompat
+import fr.nihilus.mymusic.asMediaDescription
 import fr.nihilus.mymusic.utils.MediaID
 import io.reactivex.Maybe
 import io.reactivex.Observable
@@ -99,9 +96,6 @@ class CachedMusicRepository
                 .toList()
     }
 
-    /**
-     * @return a single metadata item with the specified musicId
-     */
     override fun getMetadata(musicId: String): Single<MediaMetadataCompat> {
         val fromCache: Maybe<MediaMetadataCompat> = Maybe.fromCallable { mCache.getMetadata(musicId) }
         return fromCache.concatWith(mDao.getTrack(musicId)).firstOrError()
@@ -138,9 +132,6 @@ class CachedMusicRepository
         metadatas.map { _ -> MediaID.ID_MUSIC }
     }
 
-    /**
-     * Release all references to objects loaded by this repository.
-     */
     override fun clear() {
         mCache.clear()
     }
@@ -151,36 +142,4 @@ class CachedMusicRepository
             mCache.putMetadata(musicId, meta)
         }
     }
-}
-
-/**
- * Convert this [MediaMetadataCompat] into its [MediaDescriptionCompat] equivalent.
- * @param categories the Media ID to use a prefix for this item's Media ID
- * @param builder an optional builder for reuse
- * @return a media description created from this track metadatas
- */
-@VisibleForTesting
-internal fun MediaMetadataCompat.asMediaDescription(
-        builder: MediaDescriptionCompat.Builder,
-        vararg categories: String
-): MediaDescriptionCompat {
-    val musicId = getString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID)
-    val extras = Bundle(4)
-    extras.putString(MediaItems.EXTRA_TITLE_KEY, getString(MusicDao.CUSTOM_META_TITLE_KEY))
-    extras.putLong(MediaItems.EXTRA_DURATION, getLong(MediaMetadataCompat.METADATA_KEY_DURATION))
-    extras.putLong(MediaItems.EXTRA_TRACK_NUMBER, getLong(MediaMetadataCompat.METADATA_KEY_TRACK_NUMBER))
-    extras.putLong(MediaItems.EXTRA_DISC_NUMBER, getLong(MediaMetadataCompat.METADATA_KEY_DISC_NUMBER))
-    val artUri = getString(MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI)
-
-    val mediaId = MediaID.createMediaId(categories, musicId)
-    builder.setMediaId(mediaId)
-            .setTitle(getString(MediaMetadataCompat.METADATA_KEY_TITLE))
-            .setSubtitle(getString(MediaMetadataCompat.METADATA_KEY_ARTIST))
-            .setMediaUri(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI.buildUpon()
-                    .appendEncodedPath(musicId)
-                    .build())
-            .setExtras(extras)
-    artUri?.let { builder.setIconUri(Uri.parse(it)) }
-
-    return builder.build()
 }
