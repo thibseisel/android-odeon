b23b0323ed32e7a042935f980533cc36bbcd4dd7
Thibault Seisel <seisel.thibault@gmail.com>
2017-09-05 19:52:49 +0200

    Refactor : AlbumGridFragment uses RecyclerFragment

--- app/src/main/java/fr/nihilus/music/ui/albums/AlbumGridFragment.java
+++ app/src/main/java/fr/nihilus/music/ui/albums/AlbumGridFragment.java
@@ -6,11 +6,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityOptionsCompat;
-import android.support.v4.app.Fragment;
 import android.support.v4.media.MediaBrowserCompat.MediaItem;
 import android.support.v4.media.MediaBrowserCompat.SubscriptionCallback;
-import android.support.v4.widget.ContentLoadingProgressBar;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,19 +23,15 @@ import fr.nihilus.music.R;
 import fr.nihilus.music.di.ActivityScoped;
 import fr.nihilus.music.library.MediaBrowserConnection;
 import fr.nihilus.music.utils.MediaID;
-import fr.nihilus.music.view.GridSpacerDecoration;
+import fr.nihilus.recyclerfragment.RecyclerFragment;
 
 @ActivityScoped
-public class AlbumGridFragment extends Fragment implements AlbumsAdapter.OnAlbumSelectedListener {
+public class AlbumGridFragment extends RecyclerFragment implements AlbumsAdapter.OnAlbumSelectedListener {
 
     private static final String TAG = "AlbumGridFragment";
     private static final String KEY_ITEMS = "Albums";
 
-    private RecyclerView mRecyclerView;
     private AlbumsAdapter mAdapter;
-    private ContentLoadingProgressBar mProgressBar;
-    private View mEmptyView;
-    private ArrayList<MediaItem> mAlbums;
 
     @Inject MediaBrowserConnection mBrowserConnection;
 
@@ -46,11 +39,8 @@ public class AlbumGridFragment extends Fragment implements AlbumsAdapter.OnAlbum
         @Override
         public void onChildrenLoaded(@NonNull String parentId, @NonNull List<MediaItem> albums) {
             Log.d(TAG, "onChildrenLoaded: loaded children count: " + albums.size());
-            mAlbums.clear();
-            mAlbums.addAll(albums);
             mAdapter.updateAlbums(albums);
-            showLoading(false);
-            showEmptyView(albums.isEmpty());
+            setRecyclerShown(true);
         }
     };
 
@@ -64,13 +54,7 @@ public class AlbumGridFragment extends Fragment implements AlbumsAdapter.OnAlbum
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (savedInstanceState != null) {
-            mAlbums = savedInstanceState.getParcelableArrayList(KEY_ITEMS);
-        } else {
-            mAlbums = new ArrayList<>();
-        }
-
-        mAdapter = new AlbumsAdapter(getContext(), mAlbums);
+        mAdapter = new AlbumsAdapter(getContext(), new ArrayList<MediaItem>());
         mAdapter.setOnAlbumSelectedListener(this);
     }
 
@@ -82,46 +66,28 @@ public class AlbumGridFragment extends Fragment implements AlbumsAdapter.OnAlbum
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        mRecyclerView = view.findViewById(R.id.recyclerView);
-        mRecyclerView.addItemDecoration(new GridSpacerDecoration(getContext(),
-                getResources().getInteger(R.integer.album_grid_span_count)));
-        mProgressBar = view.findViewById(android.R.id.progress);
-        mEmptyView = view.findViewById(android.R.id.empty);
-
-        mRecyclerView.setAdapter(mAdapter);
-
-        if (savedInstanceState == null) {
-            showLoading(true);
-        }
-    }
-
-    @Override
     public void onStart() {
         super.onStart();
         getActivity().setTitle(R.string.action_albums);
         mBrowserConnection.subscribe(MediaID.ID_ALBUMS, mCallback);
+        Log.d(TAG, "Subscribed, waiting for items...");
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        setAdapter(mAdapter);
+        if (savedInstanceState == null) {
+            // Show progress indicator while loading album items
+            setRecyclerShown(false);
+        }
     }
 
     @Override
     public void onStop() {
         mBrowserConnection.unsubscribe(MediaID.ID_ALBUMS);
         super.onStop();
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        outState.putParcelableArrayList(KEY_ITEMS, mAlbums);
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onDestroyView() {
-        mRecyclerView = null;
-        mProgressBar = null;
-        mEmptyView = null;
-        super.onDestroyView();
     }
 
     @Override
@@ -132,19 +98,5 @@ public class AlbumGridFragment extends Fragment implements AlbumsAdapter.OnAlbum
         albumDetailIntent.putExtra(AlbumDetailActivity.ARG_PICKED_ALBUM, album);
         albumDetailIntent.putExtra(AlbumDetailActivity.ARG_PALETTE, holder.colors);
         startActivity(albumDetailIntent, options.toBundle());
-    }
-
-    private void showLoading(boolean shown) {
-        if (shown) {
-            mRecyclerView.setVisibility(View.GONE);
-            mProgressBar.show();
-        } else {
-            mRecyclerView.setVisibility(View.VISIBLE);
-            mProgressBar.hide();
-        }
-    }
-
-    private void showEmptyView(boolean shown) {
-        mEmptyView.setVisibility(shown ? View.VISIBLE : View.GONE);
     }
 }
