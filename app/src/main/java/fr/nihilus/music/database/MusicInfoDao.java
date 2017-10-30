a5b89d7cc4f6a244446c4e34f3e464374dd4f16d
Thibault Seisel <seisel.thibault@gmail.com>
2017-10-30 21:44:06 +0100

    Revert conversion to Kotlin

---
+++ app/src/main/java/fr/nihilus/music/database/MusicInfoDao.java
@@ -0,0 +1,14 @@
+package fr.nihilus.music.database;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.support.annotation.WorkerThread;
+
+@Dao
+public interface MusicInfoDao {
+
+    @WorkerThread
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void addStats(Iterable<MusicInfo> infos);
+}
