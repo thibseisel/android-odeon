462b745e602030c944d94ee22095497a60250318
Thibault Seisel <seisel.thibault@gmail.com>
2017-11-24 22:12:22 +0100

    Leverage PackageValidator

--- app/src/main/java/fr/nihilus/music/service/MusicService.kt
+++ app/src/main/java/fr/nihilus/music/service/MusicService.kt
@@ -63,6 +63,7 @@ class MusicService : MediaBrowserServiceCompat() {
     @Inject internal lateinit var queueManager: MediaQueueManager
     @Inject internal lateinit var errorHandler: ErrorMessageProvider<ExoPlaybackException>
 
+    private val mPackageValidator = PackageValidator(this)
     private val mDelayedStopHandler = DelayedStopHandler(this)
     private val playbackStateListener = PlaybackStateListener()
 
@@ -115,10 +116,9 @@ class MusicService : MediaBrowserServiceCompat() {
     }
 
     override fun onGetRoot(clientPackageName: String, clientUid: Int, rootHints: Bundle?): BrowserRoot? {
-        val thisApplicationPackage = application.packageName
-        if (clientPackageName != thisApplicationPackage) {
-            // The media session this app holds is private.
-            // We deny access to any app trying to connect to it.
+
+        if (!mPackageValidator.isCallerAllowed(this, clientPackageName, clientUid)) {
+            // If the request comes from an untrusted package, return null to prevent connection.
             Log.w(TAG, "onGetRoot: IGNORING request from untrusted package $clientPackageName")
             return null
         }
