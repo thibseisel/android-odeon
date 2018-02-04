abcce789fdfc7cd64c3b1494a16296cf71bcdd35
Thibault Seisel <seisel.thibault@gmail.com>
2018-02-04 11:58:15 +0100

    Improvements on ConfirmDialogFragment

--- app/src/main/java/fr/nihilus/music/utils/ConfirmDialogFragment.kt
+++ app/src/main/java/fr/nihilus/music/utils/ConfirmDialogFragment.kt
@@ -16,6 +16,7 @@
 
 package fr.nihilus.music.utils
 
+import android.app.Activity
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
@@ -33,19 +34,18 @@ import android.support.v7.app.AlertDialog
  * The supplied result code may be one of the following :
  * - [DialogInterface.BUTTON_POSITIVE] if the user selected the positive button,
  * - [DialogInterface.BUTTON_NEGATIVE] if the user selected the negative button,
- * - [DialogInterface.BUTTON_NEUTRAL] if the user selected selected the neutral button,
- * - [ConfirmDialogFragment.RESULT_DISMISSED] if the dialog is dismissed.
- *
+ * - [DialogInterface.BUTTON_NEUTRAL] if the user selected the neutral button,
+ * - [Activity.RESULT_CANCELED] if the dialog is canceled as a result of pressing back button
+ * or taping out of the dialog frame.
  */
 class ConfirmDialogFragment : DialogFragment(), DialogInterface.OnClickListener {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val args = checkNotNull(arguments) {
-            "This fragment must be created by the newInstance method"
+            "Instances of ConfirmDialogFragment should be created using the newInstance method."
         }
 
-        val builder = AlertDialog.Builder(context!!)
-            .setTitle(args.getString(ARG_TITLE))
+        val builder = AlertDialog.Builder(context!!).setTitle(args.getString(ARG_TITLE))
 
         val message = args.getString(ARG_MESSAGE)
         builder.setMessage(message)
@@ -68,9 +68,9 @@ class ConfirmDialogFragment : DialogFragment(), DialogInterface.OnClickListener 
         return builder.create()
     }
 
-    override fun onDismiss(dialog: DialogInterface?) {
-        targetFragment?.onActivityResult(targetRequestCode, RESULT_DISMISSED, null)
-        super.onDismiss(dialog)
+    override fun onCancel(dialog: DialogInterface?) {
+        targetFragment?.onActivityResult(targetRequestCode, Activity.RESULT_CANCELED, null)
+        super.onCancel(dialog)
     }
 
     override fun onClick(dialog: DialogInterface, which: Int) {
@@ -78,32 +78,27 @@ class ConfirmDialogFragment : DialogFragment(), DialogInterface.OnClickListener 
     }
 
     companion object Factory {
-        private const val ARG_TITLE = "title"
-        private const val ARG_MESSAGE = "message"
-        private const val ARG_POSITIVE = "positive_button"
-        private const val ARG_NEGATIVE = "negative_button"
-        private const val ARG_NEUTRAL = "neutral_button"
-
-        /**
-         * Result code indicating that the dialog has been dismissed by the user.
-         */
-        const val RESULT_DISMISSED = -42
+        private const val ARG_TITLE = "dialog_title"
+        private const val ARG_MESSAGE = "dialog_message"
+        private const val ARG_POSITIVE = "dialog_positive_button"
+        private const val ARG_NEGATIVE = "dialog_negative_button"
+        private const val ARG_NEUTRAL = "dialog_neutral_button"
 
         /**
          * Create a new instance of this DialogFragment.
          *
-         * @param caller the Fragment that displayed this dialog,
-         * to which the result should be forwarded
+         * @param caller the Fragment that displays this dialog
+         * to which the result should be forwarded.
          * @param requestCode a number identifying the request that'll
-         * be forwarded to [Fragment.onActivityResult] once interaction with the dialog is finished
-         * @param title the title of the dialog to display
-         * @param message an optional message to display in the dialog's body
-         * @param positiveButton an optional resource id of the text
-         * to display in the positive button. If absent, no positive button will be shown.
+         * be forwarded to [Fragment.onActivityResult] once interaction with the dialog is finished.
+         * @param title the title of the dialog to display.
+         * @param message an optional message to display as the dialog's body.
+         * @param positiveButton an optional resource id of the text.
+         * to display in the positive button. If 0, no positive button will be shown.
          * @param negativeButton an optional resource id of the text
-         * to display in the negative button. If absent, no negative button will be shown.
+         * to display in the negative button. If 0, no negative button will be shown.
          * @param neutralButton an optional resource id of the text
-         * to display in the neutral button. If absent, no neutral button will be shown.
+         * to display in the neutral button. If 0, no neutral button will be shown.
          */
         @JvmStatic fun newInstance(
             caller: Fragment?,
@@ -113,18 +108,16 @@ class ConfirmDialogFragment : DialogFragment(), DialogInterface.OnClickListener 
             @StringRes positiveButton: Int = 0,
             @StringRes negativeButton: Int = 0,
             @StringRes neutralButton: Int = 0
-        ): ConfirmDialogFragment {
-            val args = Bundle(5)
-            args.putString(ARG_TITLE, title)
-            args.putString(ARG_MESSAGE, message)
-            args.putInt(ARG_POSITIVE, positiveButton)
-            args.putInt(ARG_NEGATIVE, negativeButton)
-            args.putInt(ARG_NEUTRAL, neutralButton)
 
-            val fragment = ConfirmDialogFragment()
-            fragment.arguments = args
-            fragment.setTargetFragment(caller, requestCode)
-            return fragment
+        ) = ConfirmDialogFragment().apply {
+            setTargetFragment(caller, requestCode)
+            arguments = Bundle(5).apply {
+                putString(ARG_TITLE, title)
+                putString(ARG_MESSAGE, message)
+                putInt(ARG_POSITIVE, positiveButton)
+                putInt(ARG_NEGATIVE, negativeButton)
+                putInt(ARG_NEUTRAL, neutralButton)
+            }
         }
     }
 }
