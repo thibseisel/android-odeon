188870e7da9d9977111b82b55addf16f8f2aa44e
Thibault SEISEL <seisel.thibault@gmail.com>
2018-06-05 17:17:14 +0200

    Started writing implementation of SilenceTrimmingAudioProcessor

--- app/src/main/java/fr/nihilus/music/playback/processor/SilenceTrimmingAudioProcessor.kt
+++ app/src/main/java/fr/nihilus/music/playback/processor/SilenceTrimmingAudioProcessor.kt
@@ -16,8 +16,25 @@
 
 package fr.nihilus.music.playback.processor
 
+import android.support.annotation.IntDef
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.Format
 import com.google.android.exoplayer2.audio.AudioProcessor
 import java.nio.ByteBuffer
+
+private const val STATE_NOISY = 0
+private const val STATE_MAYBE_SILENT = 1
+private const val STATE_SILENT = 2
+
+private const val MINIMUM_SILENCE_DURATION_US = 100_000L
+private const val PADDING_SILENCE_US = 10_000L
+private const val SILENCE_THRESHOLD_LEVEL: Short = 1024
+private const val SILENCE_THRESHOLD_LEVEL_MSB = ((SILENCE_THRESHOLD_LEVEL + 128) shr 8).toByte()
+
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.TYPE, AnnotationTarget.FUNCTION)
+@IntDef(STATE_NOISY, STATE_MAYBE_SILENT, STATE_SILENT)
+private annotation class State
 
 /**
  * An [AudioProcessor] that trims silence from the start and the end of the input stream.
@@ -25,27 +42,40 @@ import java.nio.ByteBuffer
  */
 class SilenceTrimmingAudioProcessor : AudioProcessor {
 
+    private var channelCount = Format.NO_VALUE
+    private var sampleRateHz = Format.NO_VALUE
+    private var bytesPerFrame = 0
+
+    private var enabled: Boolean = false
+
+    private var buffer: ByteBuffer = AudioProcessor.EMPTY_BUFFER
+    private var outputBuffer: ByteBuffer = AudioProcessor.EMPTY_BUFFER
+    private var inputEnded = false
+
+    private var state: @State Int = STATE_NOISY
+    private var maybeSilenceBufferSize = 0
+    private var hasOutputNoise = false
+    private var skippedFrames = 0L
+
     fun setEnabled(enabled: Boolean) {
-        TODO("Not implemented")
+        this.enabled = enabled
+        flush()
     }
 
-    fun getSkippedFrames(): Long {
-        TODO("Not implemented")
-    }
-
-    override fun isActive(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun queueEndOfStream() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    fun getSkippedFrames(): Long = skippedFrames
 
     override fun configure(sampleRateHz: Int, channelCount: Int, encoding: Int): Boolean {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun getOutputEncoding(): Int {
+    override fun isActive(): Boolean = sampleRateHz != Format.NO_VALUE && enabled
+
+    override fun getOutputChannelCount(): Int = channelCount
+
+    @C.Encoding
+    override fun getOutputEncoding(): Int = C.ENCODING_PCM_16BIT
+
+    override fun queueEndOfStream() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
@@ -70,10 +100,6 @@ class SilenceTrimmingAudioProcessor : AudioProcessor {
     }
 
     override fun reset() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getOutputChannelCount(): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
