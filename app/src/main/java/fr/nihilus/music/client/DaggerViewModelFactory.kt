d30a467919647c96a6963a608bbeda45fc19bfd9
Thibault SEISEL <seisel.thibault@gmail.com>
2018-11-08 10:42:31 +0100

    More idiomatic Kotlin code in DaggerViewModelFactory

--- app/src/main/java/fr/nihilus/music/client/DaggerViewModelFactory.kt
+++ app/src/main/java/fr/nihilus/music/client/DaggerViewModelFactory.kt
@@ -19,9 +19,7 @@ package fr.nihilus.music.client
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
 import dagger.MapKey
-
 import fr.nihilus.music.di.ViewModelModule
-
 import javax.inject.Inject
 import javax.inject.Provider
 import kotlin.reflect.KClass
@@ -45,19 +43,9 @@ class DaggerViewModelFactory
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        var creator = creators[modelClass]
-        if (creator == null) {
-            for ((key, value) in creators) {
-                if (modelClass.isAssignableFrom(key)) {
-                    creator = value
-                    break
-                }
-            }
-        }
-
-        if (creator == null) {
-            throw IllegalArgumentException("Unknown model class: $modelClass")
-        }
+        val creator = creators[modelClass]
+                ?: creators.entries.firstOrNull { modelClass.isAssignableFrom(it.key) }?.value
+                ?: throw IllegalArgumentException("Unknown model class: $modelClass")
 
         @Suppress("UNCHECKED_CAST")
         return creator.get() as T
