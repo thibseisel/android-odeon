aaf1369d5d70537efb1d51c303f60d9b93a72c75
Thibault SEISEL <seisel.thibault@gmail.com>
2019-08-09 14:42:40 +0200

    Cleanup screen can now be reached from the home menu

--- app/src/main/java/fr/nihilus/music/library/HomeFragment.kt
+++ app/src/main/java/fr/nihilus/music/library/HomeFragment.kt
@@ -19,10 +19,8 @@ package fr.nihilus.music.library
 import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
