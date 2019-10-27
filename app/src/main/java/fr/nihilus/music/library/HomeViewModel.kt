6a4a3a5f9e925f076c654132a7a7fdda3d364dc9
Thibault Seisel <seisel.thibault@gmail.com>
2019-10-27 14:40:09 +0100

    Updated HomeViewModel to use the new Flow.asLiveData extension

--- app/src/main/java/fr/nihilus/music/library/HomeViewModel.kt
+++ app/src/main/java/fr/nihilus/music/library/HomeViewModel.kt
@@ -18,17 +18,13 @@ package fr.nihilus.music.library
 
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat.MediaItem
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import fr.nihilus.music.core.media.CustomActions
 import fr.nihilus.music.core.media.MediaId
 import fr.nihilus.music.core.ui.Event
 import fr.nihilus.music.core.ui.LoadRequest
 import fr.nihilus.music.core.ui.client.BrowserClient
 import fr.nihilus.music.core.ui.client.MediaSubscriptionException
-import fr.nihilus.music.core.ui.extensions.consumeAsLiveData
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -38,10 +34,10 @@ class HomeViewModel
     private val client: BrowserClient
 ) : ViewModel() {
 
-    val tracks: LiveData<LoadRequest<List<MediaItem>>> by lazyChildrenOf(MediaId.ALL_TRACKS)
-    val albums: LiveData<LoadRequest<List<MediaItem>>> by lazyChildrenOf(MediaId.ALL_ALBUMS)
-    val artists: LiveData<LoadRequest<List<MediaItem>>> by lazyChildrenOf(MediaId.ALL_ARTISTS)
-    val playlists: LiveData<LoadRequest<List<MediaItem>>> by lazyPlaylists()
+    val tracks: LiveData<LoadRequest<List<MediaItem>>> = childrenOf(MediaId.ALL_TRACKS)
+    val albums: LiveData<LoadRequest<List<MediaItem>>> = childrenOf(MediaId.ALL_ALBUMS)
+    val artists: LiveData<LoadRequest<List<MediaItem>>> = childrenOf(MediaId.ALL_ARTISTS)
+    val playlists: LiveData<LoadRequest<List<MediaItem>>> = allPlaylists()
 
     private val _deleteTracksConfirmation = MutableLiveData<Event<Int>>()
     val deleteTracksConfirmation: MutableLiveData<Event<Int>> = _deleteTracksConfirmation
@@ -68,36 +64,29 @@ class HomeViewModel
         }
     }
 
-    private fun lazyChildrenOf(parentId: String): Lazy<LiveData<LoadRequest<List<MediaItem>>>> = lazy {
-        client.getChildren(parentId)
-            .loadState()
-            .consumeAsLiveData(viewModelScope)
+    private fun allPlaylists() = combine(
+        builtInPlaylistFlow(MediaId.CATEGORY_RECENTLY_ADDED),
+        builtInPlaylistFlow(MediaId.CATEGORY_MOST_RATED),
+        client.getChildren(MediaId.TYPE_PLAYLISTS)
+    ) { mostRecent, mostRated, playlists ->
+        ArrayList<MediaItem>(playlists.size + 2).also {
+            it += mostRecent
+            it += mostRated
+            it += playlists
+        } as List<MediaItem>
     }
+        .loadState()
+        .asLiveData()
 
-    private fun lazyPlaylists(): Lazy<LiveData<LoadRequest<List<MediaItem>>>> {
-        return lazy {
-            val allPlaylists: Flow<List<MediaItem>> = combine(
-                builtInPlaylistFlow(MediaId.CATEGORY_RECENTLY_ADDED),
-                builtInPlaylistFlow(MediaId.CATEGORY_MOST_RATED),
-                client.getChildren(MediaId.TYPE_PLAYLISTS)
-            ) { mostRecent, mostRated, playlists ->
-                ArrayList<MediaItem>(playlists.size + 2).also {
-                    it += mostRecent
-                    it += mostRated
-                    it += playlists
-                }
-            }
-
-            allPlaylists
-                .loadState()
-                .consumeAsLiveData(viewModelScope)
-        }
-    }
 
     private fun builtInPlaylistFlow(category: String) = flow<MediaItem> {
         val itemId = MediaId.encode(MediaId.TYPE_TRACKS, category)
         emit(client.getItem(itemId) ?: error("Item with id $itemId should always exist."))
     }
+
+    private fun childrenOf(parentId: String) = client.getChildren(parentId)
+        .loadState()
+        .asLiveData()
 }
 
 private fun <T> Flow<T>.loadState(): Flow<LoadRequest<T>> =
