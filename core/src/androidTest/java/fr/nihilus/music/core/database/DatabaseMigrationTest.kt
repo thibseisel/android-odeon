2c93fb12d1a62a7cd22d3e44d1d4f56a8669830e
Thibault SEISEL <seisel.thibault@gmail.com>
2020-04-01 15:48:47 +0200

    Rewrite database test to use kotlin.test instead of Truth

--- core/src/androidTest/java/fr/nihilus/music/core/database/DatabaseMigrationTest.kt
+++ core/src/androidTest/java/fr/nihilus/music/core/database/DatabaseMigrationTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Thibault Seisel
+ * Copyright 2020 Thibault Seisel
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,17 +20,18 @@ import androidx.room.testing.MigrationTestHelper
 import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import com.google.common.truth.Truth.assertThat
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 private const val TEST_DB = "music.test.db"
 
 @RunWith(AndroidJUnit4::class)
 class DatabaseMigrationTest {
 
-    @[JvmField Rule]
+    @get:Rule
     val helper = MigrationTestHelper(
         InstrumentationRegistry.getInstrumentation(),
         AppDatabase::class.java.canonicalName,
@@ -47,50 +48,40 @@ class DatabaseMigrationTest {
         db.close()
 
         // Re-open the database with version 2, execute and validate the migration from v1 to v2.
-        helper.runMigrationsAndValidate(
-            TEST_DB, 2, true,
-            AppDatabase.MIGRATION_1_2
-        )
+        helper.runMigrationsAndValidate(TEST_DB, 2, true, AppDatabase.MIGRATION_1_2)
     }
 
     @Test
     fun migration2To3() {
         var db = helper.createDatabase(TEST_DB, 2)
 
-        //language=RoomSql
         db.execSQL("""
-            INSERT INTO playlist (id, title, date_created, date_last_played, art_uri)
+            INSERT INTO playlist (id, title, date_created, date_last_played, art_uri) 
             VALUES (42, 'Summertime', 1556447033, NULL, 'path/to/icon.png')
-        """.trimIndent())
-
+        """)
         db.execSQL("""
-            INSERT INTO usage_event (event_uid, track_id, event_time)
+            INSERT INTO usage_event (event_uid, track_id, event_time) 
             VALUES (12, '123', 1556447033)
-        """.trimIndent())
+        """)
 
         db.close()
-        db = helper.runMigrationsAndValidate(
-            TEST_DB, 3, false,
-            AppDatabase.MIGRATION_2_3
-        )
+        db = helper.runMigrationsAndValidate(TEST_DB, 3, false, AppDatabase.MIGRATION_2_3)
 
         // Check that the playlist has been properly migrated.
         db.query("SELECT id, title, date_created, icon_uri FROM playlist").use {
-            assertThat(it.moveToFirst()).isTrue()
-
-            assertThat(it.getLong(0)).isEqualTo(42L)
-            assertThat(it.getString(1)).isEqualTo("Summertime")
-            assertThat(it.getLong(2)).isEqualTo(1556447033L)
-            assertThat(it.getString(3)).isEqualTo("path/to/icon.png")
+            assertTrue(it.moveToFirst())
+            assertEquals(42L, it.getLong(0))
+            assertEquals("Summertime", it.getString(1))
+            assertEquals(1556447033L, it.getLong(2))
+            assertEquals("path/to/icon.png", it.getString(3))
         }
 
         // Check that usage events have been properly migrated.
         db.query("SELECT event_uid, track_id, event_time FROM usage_event").use {
-            assertThat(it.moveToFirst()).isTrue()
-
-            assertThat(it.getLong(0)).isEqualTo(12L)
-            assertThat(it.getLong(1)).isEqualTo(123L)
-            assertThat(it.getLong(2)).isEqualTo(1556447033L)
+            assertTrue(it.moveToFirst())
+            assertEquals(12L, it.getLong(0))
+            assertEquals(123L, it.getLong(1))
+            assertEquals(1556447033L, it.getLong(2))
         }
     }
 
@@ -99,7 +90,7 @@ class DatabaseMigrationTest {
         val db = helper.createDatabase(TEST_DB, 3)
         db.close()
 
-        // Check that tables have been added.
+        // Check that "remote_link" and "track_feature" tables have been added.
         helper.runMigrationsAndValidate(TEST_DB, 3, false, AppDatabase.MIGRATION_3_4)
     }
 }
