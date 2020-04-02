d319293173f932ef4f68bdf7704c562e7e22bd7a
Thibault SEISEL <seisel.thibault@gmail.com>
2020-04-02 14:21:04 +0200

    Fix inability to run instrumented tests on device/emulator

--- core/src/androidTest/java/fr/nihilus/music/core/database/DatabaseMigrationTest.kt
+++ core/src/androidTest/java/fr/nihilus/music/core/database/DatabaseMigrationTest.kt
@@ -18,17 +18,16 @@ package fr.nihilus.music.core.database
 
 import androidx.room.testing.MigrationTestHelper
 import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
 import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 private const val TEST_DB = "music.test.db"
 
-@RunWith(AndroidJUnit4::class)
+@MediumTest
 class DatabaseMigrationTest {
 
     @get:Rule
