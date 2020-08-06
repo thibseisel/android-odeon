01d72bfee533d0d684c2846a076a3e776d20be27
Thibault Seisel <seisel.thibault@gmail.com>
2020-08-06 12:41:08 +0200

    Fix deprecation of Ktor intercept by adding a parameter

--- spotify-client/src/main/java/fr/nihilus/music/spotify/service/RetryAfter.kt
+++ spotify-client/src/main/java/fr/nihilus/music/spotify/service/RetryAfter.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Thibault Seisel
+ * Copyright 2020 Thibault Seisel
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,7 +42,7 @@ internal object RetryAfter : HttpClientFeature<Unit, RetryAfter> {
 
     override fun install(feature: RetryAfter, scope: HttpClient) {
         // Add an interceptor to the send pipeline to transform the original call.
-        scope.feature(HttpSend)?.intercept { original ->
+        scope.feature(HttpSend)?.intercept { original, _ ->
             val status = original.response.status
             val retryAfterSeconds = original.response.headers[HttpHeaders.RetryAfter]?.toIntOrNull()
 
