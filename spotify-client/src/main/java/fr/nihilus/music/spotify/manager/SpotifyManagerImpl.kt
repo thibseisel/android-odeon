faf809de5c2decf1fd395f6d4be761fd1b7d79b3
Thibault Seisel <seisel.thibault@gmail.com>
2020-08-07 21:14:44 +0200

    Identify multiple tracks on Spotify in parallel

--- spotify-client/src/main/java/fr/nihilus/music/spotify/manager/SpotifyManagerImpl.kt
+++ spotify-client/src/main/java/fr/nihilus/music/spotify/manager/SpotifyManagerImpl.kt
@@ -25,12 +25,14 @@ import fr.nihilus.music.spotify.model.AudioFeature
 import fr.nihilus.music.spotify.service.HttpResource
 import fr.nihilus.music.spotify.service.SpotifyQuery
 import fr.nihilus.music.spotify.service.SpotifyService
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
 import timber.log.Timber
 import javax.inject.Inject
+
+private const val CONCURRENT_TRACK_MATCHER = 5
 
 /**
  * Implementation of the [SpotifyManager] that retrieve tracks from [MediaDao]
@@ -64,62 +66,85 @@ internal class SpotifyManagerImpl @Inject constructor(
         allTracks.await().filterNot { linksPerTrackId.containsKey(it.id) }
     }
 
-    override suspend fun sync() {
-        val unSyncedTracks = coroutineScope {
-            // Load tracks and remote links in parallel.
-            val allTracks = async { mediaDao.tracks.first() }
-            val allLinks = async { localDao.getLinks() }
+    override suspend fun sync() = coroutineScope {
+        // Load tracks and remote links in parallel.
+        val allTracks = async { mediaDao.tracks.first() }
+        val allLinks = async { localDao.getLinks() }
 
-            // Delete links pointing to a track that does not exist anymore.
-            val tracksById = allTracks.await().associateByLong { it.id }
-            val (existingLinks, oldLinks) = allLinks.await().partition { tracksById.containsKey(it.trackId) }
-            val deletedTrackIds = LongArray(oldLinks.size) { oldLinks[it].trackId }
-            localDao.deleteLinks(deletedTrackIds)
+        // Delete links pointing to a track that does not exist anymore.
+        val tracksById = allTracks.await().associateByLong { it.id }
+        val (existingLinks, oldLinks) = allLinks.await()
+            .partition { tracksById.containsKey(it.trackId) }
+        val deletedTrackIds = LongArray(oldLinks.size) { oldLinks[it].trackId }
+        localDao.deleteLinks(deletedTrackIds)
 
-            // Compute tracks that have no links.
-            val linksPerTrack = existingLinks.associateByLong { it.trackId }
-            allTracks.await().filterNot { linksPerTrack.containsKey(it.id) }
-        }
+        // Compute tracks that have no links.
+        val linksPerTrack = existingLinks.associateByLong { it.trackId }
+        val unSyncedTracks = allTracks.await().filterNot { linksPerTrack.containsKey(it.id) }
 
         Timber.tag("SpotifySync").d("%s track(s) need to be synced.", unSyncedTracks.size)
 
         if (unSyncedTracks.isNotEmpty()) {
-            val newLinks = mutableListOf<SpotifyLink>()
-
-            for (track in unSyncedTracks) {
-                val query = buildSearchQueryFor(track)
-                Timber.tag("SpotifySync").v("Searching %s", query)
-                val spotifyTrack = service.search(query).firstOrNull()
-
-                if (spotifyTrack != null) {
-                    Timber.tag("SpotifySync").v("Found track %s", spotifyTrack.name)
-                    newLinks += SpotifyLink(track.id, spotifyTrack.id, clock.currentEpochTime)
-
-                } else {
-                    Timber.tag("SpotifySync").w("Found no result for track %s.", track.title)
-                }
+            val tracks = produce(capacity = Channel.UNLIMITED) {
+                unSyncedTracks.forEach { send(it) }
             }
 
-            val trackIds = newLinks.map { it.spotifyId }
-            when (val resource = service.getSeveralTrackFeatures(trackIds)) {
-                is HttpResource.Loaded -> {
-                    for ((index, link) in newLinks.withIndex()) {
-                        val feature = resource.data[index] ?: continue
-                        localDao.saveTrackFeature(link, feature.asLocalFeature())
-                    }
-                }
+            val newLinks = produce<SpotifyLink>(capacity = Channel.BUFFERED) {
+                repeat(CONCURRENT_TRACK_MATCHER) { trackMatcher(tracks, channel) }
+            }
 
-                is HttpResource.Failed -> {
-                    Timber.tag("SpotifySync").e("Unable to fetch tracks features: HTTP error %s (%s)", resource.status, resource.message)
-                }
+            featureDownloader(newLinks)
+        }
+    }
+
+    private fun CoroutineScope.trackMatcher(
+        tracks: ReceiveChannel<Track>,
+        newLinks: SendChannel<SpotifyLink>
+    ) = launch(CoroutineName("track-matcher")) {
+        for (track in tracks) {
+            val query = SpotifyQuery.Track(title = track.title, artist = track.artist)
+            Timber.tag("SpotifySync").v("Searching %s", query)
+            val spotifyTrack = service.search(query).firstOrNull()
+
+            if (spotifyTrack != null) {
+                Timber.tag("SpotifySync").d(
+                    "Found track %s (matched %s)",
+                    spotifyTrack.name,
+                    track.title
+                )
+                val newlyFormedLink = SpotifyLink(track.id, spotifyTrack.id, clock.currentEpochTime)
+                newLinks.send(newlyFormedLink)
+
+            } else {
+                Timber.tag("SpotifySync").w("Found no result for track %s.", track.title)
             }
         }
     }
 
-    private fun buildSearchQueryFor(track: Track) = SpotifyQuery.Track(
-        title = track.title,
-        artist = track.artist
-    )
+    private fun CoroutineScope.featureDownloader(
+        links: ReceiveChannel<SpotifyLink>
+    ) = launch(CoroutineName("feature-downloader")) {
+        val newLinks = mutableListOf<SpotifyLink>()
+        links.consumeEach { newLinks += it }
+
+        val identifiedTrackIds = newLinks.map { it.spotifyId }
+        when (val resource = service.getSeveralTrackFeatures(identifiedTrackIds)) {
+            is HttpResource.Loaded -> {
+                for ((index, link) in newLinks.withIndex()) {
+                    val feature = resource.data[index] ?: continue
+                    localDao.saveTrackFeature(link, feature.asLocalFeature())
+                }
+            }
+
+            is HttpResource.Failed -> {
+                Timber.tag("SpotifySync").e(
+                    "Unable to fetch tracks features: HTTP error %s (%s)",
+                    resource.status,
+                    resource.message
+                )
+            }
+        }
+    }
 
     private fun AudioFeature.asLocalFeature() = TrackFeature(
         id = id,
