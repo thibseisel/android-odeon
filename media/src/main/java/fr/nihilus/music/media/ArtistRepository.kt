d312ed07de08107c0f0fe50bde71a9b33b12b14f
Thibault SEISEL <seisel.thibault@gmail.com>
2022-04-28 14:54:20 +0200

    Define ArtistRepository
    This repository exposes a live list of albums.
    It depends on:
    - ArtistLocalSource, that implements artist-specific operations from MediaDao,
    - TrackRepository, to take excluded tracks into account.

---
+++ media/src/main/java/fr/nihilus/music/media/ArtistRepository.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2022 Thibault Seisel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.nihilus.music.media
+
+import fr.nihilus.music.media.provider.Artist
+import fr.nihilus.music.media.provider.ArtistLocalSource
+import fr.nihilus.music.media.provider.Track
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import javax.inject.Inject
+
+class ArtistRepository @Inject internal constructor(
+    private val artistSource: ArtistLocalSource,
+    private val trackRepository: TrackRepository,
+) {
+    /**
+     * Live list of all artists that contributed to tracks in the music library.
+     * The list of artists is sorted alphabetically by [their name][Artist.name].
+     */
+    val artists: Flow<List<Artist>>
+        get() = combine(artistSource.artists, trackRepository.tracks) { allArtists, tracks ->
+            val tracksPerArtist = tracks.groupBy(Track::artistId)
+            allArtists.mapNotNull { artist ->
+                val artistTracks = tracksPerArtist[artist.id].orEmpty()
+
+                when (val trackCount = artistTracks.size) {
+                    0 -> null
+                    artist.trackCount -> artist
+                    else -> {
+                        val albumCount = artistTracks.distinctBy(Track::albumId).size
+                        artist.copy(trackCount = trackCount, albumCount = albumCount)
+                    }
+                }
+            }
+        }
+}
---
+++ media/src/test/java/fr/nihilus/music/media/provider/ArtistFixtures.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2022 Thibault Seisel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.nihilus.music.media.provider
+
+internal val ACDC = Artist(
+    id = 5,
+    name = "AC/DC",
+    trackCount = 2,
+    albumCount = 1,
+    iconUri = null,
+)
+
+internal val ALESTORM = Artist(
+    id = 26,
+    name = "Alestorm",
+    trackCount = 1,
+    albumCount = 1,
+    iconUri = null
+)
+
+internal val AVENGED_SEVENFOLD = Artist(
+    id = 4,
+    name = "Avenged Sevenfold",
+    trackCount = 1,
+    albumCount = 1,
+    iconUri = null,
+)
+
+internal val FOO_FIGHTERS = Artist(
+    id = 13,
+    name = "Foo Fighters",
+    trackCount = 4,
+    albumCount = 3,
+    iconUri = null,
+)
+
+internal val MUSE = Artist(
+    id = 18,
+    name = "Muse",
+    trackCount = 2,
+    albumCount = 2,
+    iconUri = null
+)
