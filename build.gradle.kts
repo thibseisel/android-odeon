1d07b167e1857c8dacfc066982efbf113a71611e
Thibault Seisel <seisel.thibault@gmail.com>
2023-09-09 11:02:53 +0200

    refactor: use Room compiler with KSP

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.ksp) apply false
     alias(libs.plugins.androidx.navigation.safeargs.kotlin) apply false
 }
 
